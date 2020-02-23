20ec7a0a2c0228fa8530f202bc1b3d9a7566c77c
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2020-02-23 23:31:38 +0700

    Completed constructor and getter setter of Student Class

--- src/main/java/Student_Class/Student.java
+++ src/main/java/Student_Class/Student.java
@@ -11,4 +11,58 @@ package Student_Class;
  */
 public class Student {
     
+    public static String school = "HCMUS";
+    
+    private String name;
+    private String id;
+    private String address;
+    private int age;
+    private float gpa;
+    //---Constructor
+    public Student(){
+    }
+    
+    public Student(String name, String id, String address, int age, float gpa){
+        this.name=name;
+        this.address=address;
+        this.age=age;
+        this.gpa=gpa;
+        this.id=id;
+    }
+    //---Getter vs Setter
+    //---name
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    //---Address
+    public String getAddress(){
+        return this.address;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    //---Age
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    //---GPA
+    public float getGPA(){
+        return this.gpa=gpa;
+    }
+    public void setGPA(float gpa){
+        this.gpa=gpa;
+    }
+    //---ID
+    public String getID(){
+        return this.id;
+    }
+    public void setID(String id){
+        this.id=id;
+    }
 }
